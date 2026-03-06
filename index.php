05983c7ec015b6e9ad8c64521344b4f4551a4230
KakoAro <k.balio.146235.tc@umindanao.edu.ph>
2026-03-06 15:16:24 +0800

    Unfinished na code

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h2>Personal Profile Generator<h2>
+            <form action="profile.php" method="POST" enctype="multipart/form-data">
+
+                <!--Question 1-->
+                <label> 1. Full Name: </label><br>
+                <input type="text" name="fullname" required><br><br>
+
+
+
+
+</body>
+
+</html>
