bc667a6fab622df89f4fb4761e5cbe3e462cbd86
KakoAro <k.balio.146235.tc@umindanao.edu.ph>
2026-03-07 23:13:07 +0800

    final na edit

--- profile.php
+++ profile.php
@@ -1,24 +1,53 @@
-<<<<<<< HEAD
+<?php
+
+// Retrieve form data 
+$fullname = $_POST['fullname'];
+$age = $_POST['age'];
+$course = $_POST['course'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
+$bio = $_POST['bio'];
+
+$hobbies = "";
+if(isset($_POST['hobbies'])){
+    $hobbies = implode(", ", $_POST['hobbies']);
+}
+
+// Image upload 
+$imageName = $_FILES['profile_image']['name'];
+$tempName = $_FILES['profile_image']['tmp_name'];
+
+$uploadFolder = "uploads/";
+
+if(!file_exists($uploadFolder)){
+    mkdir($uploadFolder);
+}
+
+$targetPath = $uploadFolder . $imageName;
+
+// Move uploaded image
+move_uploaded_file($tempName, $targetPath);
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>User Profile</title>
 </head>
 <body>
-    
+
+<h2>User Profile</h2>
+
+<img src="<?php echo $targetPath; ?>" width="200"><br><br>
+
+<p><b>1. Full Name:</b> <?php echo $fullname; ?></p>
+<p><b>2. Age:</b> <?php echo $age; ?></p>
+<p><b>3. Course / Program:</b> <?php echo $course; ?></p>
+<p><b>4. Email:</b> <?php echo $email; ?></p>
+<p><b>5. Gender:</b> <?php echo $gender; ?></p>
+<p><b>6. Hobbies:</b> <?php echo $hobbies; ?></p>
+<p><b>7. Short Biography:</b> <?php echo $bio; ?></p>
+
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
->>>>>>> 712ee1c8c251933bc77b19f3f44cbaa74f217b8b
 </html>
